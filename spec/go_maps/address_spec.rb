2727bcb1de11aa0219e7c0d99f1a908d7707bc90
Diego Carrion <dc.rec1@gmail.com>
2010-07-09 20:08:59 -0300

    small refactor

--- spec/go_maps/address_spec.rb
+++ spec/go_maps/address_spec.rb
@@ -1,6 +1,6 @@
 require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 
-describe 'GoMaps::Address' do
+describe GoMaps::Address do
   context 'on #exists?' do
     it 'should return true given a valid address' do
       address = 'Valid Address'
