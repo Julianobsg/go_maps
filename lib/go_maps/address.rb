d8c26d5c25d0665855f41f1d1c78e231fab0bdec
BrunoGrasselli <bruno.grasselli@gmail.com>
2010-07-11 02:24:59 -0300

    Using ['distance']['value'] instead of ['distance']['text'] to calculate distances.

--- lib/go_maps/address.rb
+++ lib/go_maps/address.rb
@@ -9,7 +9,7 @@ module GoMaps
     end
 
     def distance_to(address)
-      directions_to(address)['routes'].first['legs'].first['distance']['text'].to_f rescue raise AddressNotFoundException
+      directions_to(address)['routes'].first['legs'].first['distance']['value'] rescue raise AddressNotFoundException
     end
 
     private
