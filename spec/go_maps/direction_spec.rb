23bb3c1262f6d4866ba0d855e2f5366fd6d9a4ac
BrunoGrasselli <bruno.grasselli@gmail.com>
2010-07-14 13:35:25 -0300

    Refactor

--- spec/go_maps/direction_spec.rb
+++ spec/go_maps/direction_spec.rb
@@ -4,18 +4,19 @@ describe GoMaps::Direction do
   context 'on #to_map' do
     before :each do
       @address1, @address2 = 'Invalid Address 1', 'Invalid Address 2'
+      @direction = GoMaps::Direction.new(:from => @address1, :to => @address2)
     end
 
     it 'should return a map' do
-      GoMaps::Direction.new(:from => @address1, :to => @address2).to_map.should eql("<iframe width='425' height='350' frameborder='0' scrolling='no' marginheight='0' marginwidth='0' src='http://maps.google.com/maps?ie=UTF8&output=embed&saddr=#{@address1}&daddr=#{@address2}'></iframe>")
+      @direction.to_map.should eql("<iframe width='425' height='350' frameborder='0' scrolling='no' marginheight='0' marginwidth='0' src='http://maps.google.com/maps?ie=UTF8&output=embed&saddr=#{@address1}&daddr=#{@address2}'></iframe>")
     end
 
     it 'should accept width as an option' do
-      GoMaps::Direction.new(:from => @address1, :to => @address2).to_map(:width => 333).include?('width=\'333\'').should be_true
+      @direction.to_map(:width => 333).include?('width=\'333\'').should be_true
     end
 
     it 'should accept height as an option' do
-      GoMaps::Direction.new(:from => @address1, :to => @address2).to_map(:height => 444).include?('height=\'444\'').should be_true
+      @direction.to_map(:height => 444).include?('height=\'444\'').should be_true
     end
   end
 end
