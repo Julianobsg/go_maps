600424fe96b4feec00a05669cbde772aa8c6ea4d
Diego Carrion <dc.rec1@gmail.com>
2011-04-14 11:16:52 -0300

    updated cep service

--- lib/go_maps/cep.rb
+++ lib/go_maps/cep.rb
@@ -1,12 +1,12 @@
 module GoMaps
   class CEP
     def initialize(cep)
-      @response = Nokogiri::XML(open("http://www.bronzebusiness.com.br/webservices/wscep.asmx/cep?strcep=#{cep}"))
-      raise GoMaps::AddressNotFoundException if element('tbCEP').blank?
+      @response = Nokogiri::XML(open("http://cep.republicavirtual.com.br/web_cep.php?cep=#{cep}"))
+      raise GoMaps::AddressNotFoundException if element('resultado') == "0"
     end
 
     def street
-      "#{element('logradouro')} #{element('nome')}"
+      "#{element('tipo_logradouro')} #{element('logradouro')}"
     end
 
     def city
--- spec/go_maps/cep_spec.rb
+++ spec/go_maps/cep_spec.rb
@@ -3,7 +3,7 @@ require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 describe GoMaps::CEP do
   context 'given valid cep' do
     before :each do
-      map_url_to_file 'http://www.bronzebusiness.com.br/webservices/wscep.asmx/cep?strcep=01310-000', 'cep_success'
+      map_url_to_file 'http://cep.republicavirtual.com.br/web_cep.php?cep=01310-000', 'cep_success'
       @cep = GoMaps::CEP.new('01310-000')
     end
 
@@ -15,14 +15,14 @@ describe GoMaps::CEP do
 
     context 'on #city' do
       it 'should return the city' do
-        @cep.city.should eql('Sao Paulo')
+        @cep.city.should eql('São Paulo')
       end
     end
   end
 
   context 'given invalid cep' do
     it 'should raise AddressNotFoundException' do
-      map_url_to_file 'http://www.bronzebusiness.com.br/webservices/wscep.asmx/cep?strcep=12345-678', 'cep_error'
+      map_url_to_file 'http://cep.republicavirtual.com.br/web_cep.php?cep=12345-678', 'cep_error'
       lambda { @cep = GoMaps::CEP.new('12345-678') }.should raise_error(GoMaps::AddressNotFoundException)
     end
   end
